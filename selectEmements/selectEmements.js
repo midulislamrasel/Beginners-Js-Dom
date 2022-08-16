af1ffcdb2f5cb2c7a764ec67f176f62ac8329066
Midul-islam-Russell <midulraddy121@gmail.com>
2022-08-16 15:33:18 +0600

    Beginner Js Dom

---
+++ selectEmements/selectEmements.js
@@ -0,0 +1,18 @@
+//getElementBYId
+document.getElementById("heading").innerHTML = " getElementBYId "
+
+//getElementsByClassName
+document.getElementsByClassName("heading2")[0].innerHTML = " getElementsByClassName"
+
+//getElementsByTagName
+document.getElementsByTagName("h1")[2].innerHTML ="  getElementsByTagName"
+
+//querySelector class
+document.querySelector(".cLass").innerHTML = " testing querySelector class"
+
+//querySelector Id
+document.querySelector("#ID").innerHTML = " testing querySelector id"
+
+//querySelector Id2
+document.querySelector("#ID2").innerHTML=" testing querySelector  id2"
+
