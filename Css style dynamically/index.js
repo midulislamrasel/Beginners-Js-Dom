e85b30d598816aa045e80c4266dbb19492ce3afb
Midul-islam-Russell <midulraddy121@gmail.com>
2022-08-17 09:39:22 +0600

    css style dynamically

---
+++ Css style dynamically/index.js
@@ -0,0 +1,11 @@
+
+var myPera = document.getElementById("pera");
+
+function styleButton() {
+    // myPera.style.fontSize = " 4rem";
+    myPera.classList.add("stylePara");
+}
+
+function NOstyleButton() {
+    myPera.classList.remove("stylePara");
+}
