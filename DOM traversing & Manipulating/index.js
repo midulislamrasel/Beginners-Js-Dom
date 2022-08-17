61d485be0fbdbaa1424d727f14b623b0794ac9d0
Midul-islam-Russell <midulraddy121@gmail.com>
2022-08-17 08:22:39 +0600

    DOM traversing

---
+++ DOM traversing & Manipulating/index.js
@@ -0,0 +1,20 @@
+//DOM full Name : Document Object Model
+
+//Direction for traversing
+/*Downwards
+1. querySelector() / querySelectorAll()
+2. children */
+
+/*Upwards
+    1. ParentElement*/
+
+/*Sideways
+    1.nextElementSibling,
+    previousElementSibling
+    2.ParentElement + Children + index */
+
+
+// --------------
+//Direction for traversin
+
+
